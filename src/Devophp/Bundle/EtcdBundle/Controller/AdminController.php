72321adb642c2ba4aeaf660f526ab79fa8a9f812
j.faassen <j.faassen@linkorb.com>
2014-02-02 21:04:49 +0100

    Updated EtcdBundle to list/add/view clusters using Doctrine entities

--- src/Devophp/Bundle/EtcdBundle/Controller/AdminController.php
+++ src/Devophp/Bundle/EtcdBundle/Controller/AdminController.php
@@ -11,6 +11,8 @@ use Acme\DemoBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Devophp\Bundle\EtcdBundle\Entity\Cluster;
+
 class AdminController extends Controller
 {
     /**
@@ -19,16 +21,46 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        $em = $this->container->get('doctrine')->getManager();
+        $repo = $em->getRepository('DevophpEtcdBundle:Cluster');
+
+        $clusters = $repo->findAll();
+
+
+        return array('clusters' => $clusters);
     }
 
     /**
-     * @Route("/hello/{name}", name="_demo_hello")
+     * @Route("/etcdadmin/addcluster", name="_devophp_etcdadmin_addcluster")
+     */
+    public function addClusterAction()
+    {
+        $cluster = new Cluster();
+        $cluster->setName('Darth\'s surprise birthday party');
+        $cluster->setAddress('http://127.0.0.1:4001');
+        $cluster->setDescription('Local dev etcd cluster');
+        $em = $this->container->get('doctrine')->getManager();
+        $em->persist($cluster);
+        $em->flush();
+        $clusterid =$cluster->getId();
+        return $this->redirect($this->generateUrl('_devophp_etcdadmin_editcluster', array('clusterid'=> $clusterid)));
+
+    }
+
+    /**
+     * @Route("/etcdadmin/editcluster/{clusterid}", name="_devophp_etcdadmin_editcluster")
      * @Template()
      */
-    public function helloAction($name)
+    public function editClusterAction($clusterid)
     {
-        return array('name' => $name);
+        $em = $this->container->get('doctrine')->getManager();
+        $repo = $em->getRepository('DevophpEtcdBundle:Cluster');
+
+        $cluster = $repo->findOneBy(array(
+            'id' => $clusterid,
+        ));
+
+        return array('clusterid' => $clusterid, 'cluster' => $cluster);
     }
 
 }
