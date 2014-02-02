2c869553559db655b3d77bbefc7eebe3cd620a2a
j.faassen <j.faassen@linkorb.com>
2014-02-02 15:43:42 +0100

    Create DevophpEtcdBundle

---
+++ src/Devophp/Bundle/EtcdBundle/Controller/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Devophp\Bundle\EtcdBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Acme\DemoBundle\Form\ContactType;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/", name="_index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/hello/{name}", name="_demo_hello")
+     * @Template()
+     */
+    public function helloAction($name)
+    {
+        return array('name' => $name);
+    }
+
+}
---
+++ src/Devophp/Bundle/EtcdBundle/DevophpEtcdBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Devophp\Bundle\EtcdBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DevophpEtcdBundle extends Bundle
+{
+}
